bbd0991faefd6cf65f22d1bf8fab1d12ea9363c6
Tongxin Sun <37636358+Tongxin-Sun@users.noreply.github.com>
2023-05-24 00:38:00 -0700

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,19 +5,19 @@ function HomePage() {
     return (
         <>
             <div className="header">
-                <div class="title">
-                    <div class="title-inner">
-                        <div class="cafe">
-                            <div class="cafe-inner">Keyframé</div>
+                <div className="title">
+                    <div className="title-inner">
+                        <div className="cafe">
+                            <div className="cafe-inner">Keyframé</div>
                         </div>
-                        <div class="mozart">
-                            <div class="mozart-inner">Artistes</div>
+                        <div className="mozart">
+                            <div className="mozart-inner">Artistes</div>
                         </div>
                     </div>
                 </div>
             
                 <div className="image">
-                    <img src='https://images.unsplash.com/photo-1616362355051-6a9f8c434fff?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNzE0MTYzNQ&ixlib=rb-1.2.1&q=80&w=800&h=600' alt=''className="background-image"/>
+                    <img className="background-image" src='https://images.unsplash.com/photo-1616362355051-6a9f8c434fff?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNzE0MTYzNQ&ixlib=rb-1.2.1&q=80&w=800&h=600' alt=''/>
                 </div>
             </div>
             {/*<h2><i><AiFillHome/></i>WELCOME!</h2>
